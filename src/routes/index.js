0a2716e6e017b450682297438c321afa84dde3ce
Stephen <hellostephenwm@gmail.com>
2022-08-13 23:54:43 -0400

    Added list of other api endpoints

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,13 @@ const router = require('express').Router()
 router.get('/', (req, res) => {
     res.status(200).json({
         status: 200,
-        message: 'Index endpoint'
+        message: 'Index endpoint',
+        allEndPoints: {
+            'Auth': '/api/auth',
+            'Rooms': '/api/rooms',
+            'hotels': '/api/hotels',
+            'users': '/api/users'
+        }
     })
 })
 
