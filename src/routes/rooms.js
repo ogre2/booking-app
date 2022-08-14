5cf51d701688a3547935a0f1d4da2468b9769c9c
Stephen <hellostephenwm@gmail.com>
2022-08-13 23:54:58 -0400

    Modified text

--- src/routes/rooms.js
+++ src/routes/rooms.js
@@ -3,7 +3,7 @@ const router = require('express').Router()
 router.get('/', (req, res) => {
     res.status(200).json({
         status: 200,
-        message: 'Hotels endpoint'
+        message: 'Rooms endpoint'
     })
 })
 
