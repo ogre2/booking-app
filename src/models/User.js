d8f32e1666d35cf081c9738b8e3fd14fe64102f6
Stephen <hellostephenwm@gmail.com>
2022-08-14 21:20:59 -0400

    Created User model

---
+++ src/models/User.js
@@ -0,0 +1,36 @@
+// Importing express
+const mongoose = require('mongoose')
+
+// Creating the User schema
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    userName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    emailAddress: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('User', UserSchema)
