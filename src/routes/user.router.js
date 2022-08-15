48db2d68ff1c93b546fc671459f39d65be2b663d
Stephen <hellostephenwm@gmail.com>
2022-08-14 21:26:32 -0400

    Reformatted to use the user controller functions

---
+++ src/routes/user.router.js
@@ -0,0 +1,8 @@
+// Importing express router
+const router = require('express').Router()
+// Importing controller
+const controller = require('../controllers/userController.js')
+
+
+// Exporting router
+module.exports = router
